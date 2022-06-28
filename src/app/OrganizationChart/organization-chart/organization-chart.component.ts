14c921d6f9e2deb4973927371be4343c9ec4bcb7
rbahena <rbahena@cjf.gob.mx>
2022-06-27 19:40:32 -0500

    Add organization chart component

---
+++ src/app/OrganizationChart/organization-chart/organization-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-chart',
+  templateUrl: './organization-chart.component.html',
+  styleUrls: ['./organization-chart.component.css']
+})
+export class OrganizationChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrganizationChartComponent } from './OrganizationChart/organization-chart/organization-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizationChartComponent
   ],
   imports: [
     BrowserModule,
